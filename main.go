270ebbc6769e5c914e3ce76102facc0d41644737
devstackq <loginpsn@mail.ru>
2021-10-15 13:39:49 +0600

    step2 : validUrl, createShortUrl, Randomaizer funcs

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
+	"regexp"
 	"time"
 )
 
@@ -15,43 +16,76 @@ func main() {
 	http.ListenAndServe(":9000", nil)
 }
 
-type Data struct {
-	Url       string
-	ShortLink string
+type UrlData struct {
+	Url      string `json:"url"`
+	ShortUrl string `json:"short_url"`
+	Host     string
 }
 
-// curl -H "Content-Type: application/json" --request POST  --data '{"url":"https://github.com/destackq/real-time-forum" }'  http://localhost:9000
+//post   http://localhost:9000/ body : {url: https://github.com/devstackq/ozon-task }
+//get http://localhost:9000/?url=gb.com/FvRIStQgV8
+
+var UrlStruct UrlData
 
 func urlHandler(w http.ResponseWriter, r *http.Request) {
 	//domain name -> get query
 	switch r.Method {
+
 	case "GET":
 		//get my server host url, r.URL.Query() - get after /, value(short link)
 		//get from redis data key:value, key - shor link - value link
-		for k, v := range r.URL.Query() {
-			fmt.Printf("%s: %s\n", k, v)
+
+		keys, ok := r.URL.Query()["url"]
+
+		if !ok || len(keys[0]) < 1 {
+			log.Println("url param 'url' is missing or incorrect")
+			return
 		}
+
+		if err, shortUrl := UrlStruct.IsUniqUrl(); err == nil && shortUrl != "" {
+			//return full url from db || redis
+			w.WriteHeader(200)
+			w.Write([]byte(shortUrl))
+			return
+		}
+
+		w.WriteHeader(400)
+		w.Write([]byte(http.StatusText(http.StatusBadRequest)))
+		return
+
 		//get json : url -> validUrl,  generate random short link, check isUniq in Db ? -> save Redis & Db
 	case "POST":
-		d := Data{}
 
 		err := json.NewDecoder(r.Body).Decode(&d)
 		if err != nil {
 			log.Println(err)
 		}
-		// fmt.Printf("%s\n url value from client", d.Url)
 
 		//check valid url ?
-		if ok := d.IsValidUrl(d.Url); ok {
-			d.Randomaizer()
-			log.Print(d, "data")
-			if ok := d.IsUniqUrl(); ok {
-				//save redis, then db
-			}
-		}
+		if err, host := UrlStruct.IsValidUrl(UrlStruct.Url); err == nil {
+			// check isUrlExist ?
+			if err, shortUrl := UrlStruct.IsUniqUrl(); err == nil && shortUrl == "" {
+				if err := UrlStruct.CreateShortHost(host); err == nil {
+					UrlStruct.Randomaizer()
 
-		w.WriteHeader(200)
-		w.Write([]byte(d.ShortLink))
+					if UrlStruct.ShortUrl != "" {
+						fmt.Println(d, "create short url")
+						//save redis, then db, return json. new Url
+						w.WriteHeader(200)
+						w.Write([]byte(UrlStruct.ShortUrl))
+						return
+					}
+				}
+			} else {
+				w.WriteHeader(200)
+				//get from db || redis by d.Url - shortUrl
+				//return generated url by key
+				w.Write([]byte(shortUrl))
+			}
+			w.WriteHeader(500)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
 	default:
 		w.WriteHeader(http.StatusNotImplemented)
 		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
@@ -59,10 +93,26 @@ func urlHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//set new short host
+func (d *UrlData) CreateShortHost(host string) error {
+
+	re, err := regexp.Compile(`[.]`)
+	if err != nil {
+		return err
+	}
+
+	hostSplit := re.Split(host, -1)
+
+	if len(hostSplit) == 2 {
+		d.Host = string(hostSplit[0][0]) + string(hostSplit[0][len(hostSplit[0])-1]) + "." + hostSplit[1] + "/"
+	} else if len(hostSplit) == 3 {
+		d.Host = string(hostSplit[0]) + "." + string(hostSplit[1][0]) + string(hostSplit[1][len(hostSplit[1])-1]) + "." + hostSplit[2] + "/"
+	}
+	return nil
+}
 
 //62 elem, 450penta variance
-
-func (d *Data) Randomaizer() {
+func (d *UrlData) Randomaizer() {
 
 	items := [62]int{}
 
@@ -86,20 +136,20 @@ func (d *Data) Randomaizer() {
 		short += string(items[r1.Intn(62)])
 	}
 
-	log.Println(short, "short link create")
-	d.ShortLink = short
-}
-
-func (d *Data) IsUniqUrl() bool {
-	//check redis, then db
-	return true
-}
-
-func (d *Data) IsValidUrl(host string) bool {
-	_, err := url.ParseRequestURI(d.Url)
-	if err != nil {
-		log.Println(err)
-		return false
+	if len(short) == 10 && short != "" {
+		d.ShortUrl = d.Host + short
 	}
-	return true
+}
+
+func (d *UrlData) IsUniqUrl() (error, string) {
+	//check redis, then db
+	return nil, ""
+}
+
+func (d *UrlData) IsValidUrl(host string) (error, string) {
+	u, err := url.ParseRequestURI(d.Url)
+	if err != nil {
+		return err, ""
+	}
+	return nil, u.Hostname()
 }
